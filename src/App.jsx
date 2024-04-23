739030150c6a1d33912660a3684b6194808f9d3e
saurabhHD <saurabhinpurani@gmail.com>
2024-04-23 07:37:47 +0530

    add components like header, footer and also init some methods in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+import { useDispatch } from "react-redux"
+import {login, logout} from "./features/auth/authSlice"
+import { useEffect, useState } from "react"
+import authService from "./appwrite/auth"
+import { Header, Footer } from "./components"
 
 function App() {
+
+  const [loading, setLoding] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login(userData))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .catch(()=>{
+      dispatch(logout())
+    })
+    .finally(()=>{
+      setLoding(false)
+    })
+  }, [])
+
   return (
     <div>
-      Hwllo
+      <Header />
+      Hwllo {loading}
+      <Footer />
     </div>
   )
 }
---
+++ src/components/header/index.jsx
@@ -0,0 +1,9 @@
+const Header = ()=>{
+    return (
+        <div>
+            This is header
+        </div>
+    )
+}
+
+export default Header
